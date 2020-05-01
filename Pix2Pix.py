8386f50ac30c7238526f402d60674998a47d6cb3
sangeetsu <43584744+sangeetsu@users.noreply.github.com>
2020-05-01 07:35:36 -0500

    Update Pix2Pix.py

--- Pix2Pix.py
+++ Pix2Pix.py
@@ -24,6 +24,9 @@ import os
 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
+tf.config.experimental.set_memory_growth(physical_devices[1], True)
+tf.config.experimental.set_memory_growth(physical_devices[2], True)
+tf.config.experimental.set_memory_growth(physical_devices[3], True)
 
 # define the discriminator model
 def define_discriminator(image_shape):
